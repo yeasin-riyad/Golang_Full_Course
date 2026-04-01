96d99f4ae62d9de4044dac9bb422d2e893ab65ef
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-04-01 18:45:41 +0600

    012_switch_statement

---
+++ 1_golang_basics/012_switch_statement/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	day := 3
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	default:
+		fmt.Println("Unknown day")
+	}
+}
