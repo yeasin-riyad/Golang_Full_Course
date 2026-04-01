29eaaf80d76d8ad390076b6add12ea2ddac0acbd
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-04-01 14:32:10 +0600

    02_variables_and_types

---
+++ 1_golang_basics/02_variables_and_types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name type
+	var channelName string
+	channelName = "AceDevHub"
+
+	var year int = 2026
+
+	fmt.Println("Channel:", channelName)
+	fmt.Println("Year:", year)
+
+	var rating float64 = 4.8
+	fmt.Println("rating:", rating)
+
+}
