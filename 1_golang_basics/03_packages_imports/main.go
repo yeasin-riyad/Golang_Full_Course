0b450ac2cfbba886c793dd6195c437c49c613c82
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-04-01 14:34:24 +0600

    03_packages_imports

---
+++ 1_golang_basics/03_packages_imports/main.go
@@ -0,0 +1,16 @@
+package main
+
+// imports brings external packages into the file that u r working
+// where actually u need it
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// packageName.Functionname -> call a function from a package
+
+	fmt.Println("sqrt(25)", math.Sqrt(25))
+
+}
